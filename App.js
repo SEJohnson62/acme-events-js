c54aed09743991510124e4e362e30c3fcf79182d
Susan E. Johnson <seagull62@icloud.com>
2020-01-20 17:52:07 -0800

    create and display events

--- App.js
+++ App.js
@@ -1,11 +1,40 @@
 import React, {useState} from 'react';
 import './App.css';
 
+const EventList = ({ plans, changePlans }) => {
+  console.log( plans );
+  return (
+    <ul>
+      {
+        plans.map( (plan, idx) => {
+          return (
+            <li key={ idx } >
+             {plan.title}
+            </li>
+          );
+        })
+      }
+    </ul>
+  );
+};
+
+function CreateEvent ({onChange, onSubmit, plan}) {
+  return(
+    <form id="today" onSubmit={ onSubmit }>
+        <input name='date' value={ plan.date } onChange={onChange}/>
+        <input name='title' value={ plan.title }onChange={onChange}/>
+        <input name='content' value={ plan.content } onChange={onChange}/>
+        <button disabled={!plan.title && !plan.content}>Save Event</button>
+    </form>
+  );
+}
 
 function App() {
+
+  // Initialize header
   const today = new Date();
-  console.log( today );
   const currentDay = today.getDay();
+
   let dayString = '';
   switch ( currentDay ) {
     case 0: dayString = 'Sunday';
@@ -24,31 +53,33 @@ function App() {
       break;
     default:dayString = 'Saturday';
   }
-  console.log(dayString);
-  const [ plans, setPlans] = useState([]);
-  const [ plan, setPlan ] = useState({date: today, title: '', content: ''});
+
+  // Initialize useState
+  const [ plans, setPlans ] = useState([]);
+  let [ plan, setPlan] = useState({date: '', title: '', content:''});
+
+  // Initialize event handlers
   const onChange = (ev)=> {
-    console.log("In onChange")
-    const change = {};
-    change[ev.target.name] = ev.target.value;
-    setPlan({...setPlan, ...change})
+    console.log("In onChange", ev.target);
+    setPlan({...plan, [ev.target.name]:ev.target.value });
   }
+
   const onSubmit = (ev)=> {
     ev.preventDefault();
-    console.log("In onSubmit")
-    setPlans([...plans, plan]);
+    console.log("In onSubmit", ev.target);
+    const date = new Date( plan.date );
+    setPlans([...plans, {...plan, date}]);
   }
+
   return (
     <div className="App">
       <h1>The Acme Event Site</h1>
       <h2> Today is: {dayString}</h2>
-      <form id="today" onSubmit={ onSubmit }>
-        <input name='date' value= {today} onChange={onChange}/>
-        <input name='title' value={ plan.title }onChange={onChange}/>
-        <input name='content' value={ plan.content } onChange={onChange}/>
-        <button disabled={!plan.title && !plan.content}>Save Event</button>
-      </form>
+        <CreateEvent onChange={onChange} onSubmit={onSubmit} plan={plan}/>
+
+      <h2> All {plans.length} Events:</h2>
       <form id="future">
+        <EventList plans= { plans }/>
       </form>
       <form id="past">
       </form>
