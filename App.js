ded3770d47538d6f1c0153f5509ae082c86b02c6
Susan E. Johnson <seagull62@icloud.com>
2020-01-21 16:08:59 -0800

    added Sort Events Button

--- App.js
+++ App.js
@@ -1,15 +1,14 @@
 import React, {useState} from 'react';
 import './App.css';
 
-const EventList = ({ plans, changePlans }) => {
-  console.log( plans );
+const EventList = ({ plans }) => {
   return (
     <ul>
       {
         plans.map( (plan, idx) => {
           return (
-            <li key={ idx } >
-              <p>{plan.date.toDateString()} {plan.title}</p>
+            <li key={ idx }>
+              { plan.date.toDateString() } { plan.title }>
             </li>
           );
         })
@@ -33,6 +32,8 @@ function App() {
 
   // Initialize header
   const today = new Date();
+  const year = today.getFullYear();
+  console.log("year = ", typeof(year));
   const currentDay = today.getDay();
 
   let dayString = '';
@@ -67,8 +68,30 @@ function App() {
   const onSubmit = (ev)=> {
     ev.preventDefault();
     console.log("In onSubmit", ev.target);
-    const date = new Date( plan.date );
+    // validate input
+
+    let date = new Date( plan.date );
     setPlans([...plans, {...plan, date}]);
+  }
+  function groupByDate(){
+    function compareNumbers( numOne, numTwo ) {
+      if( numOne < numTwo ) {
+        return -1;
+      } else if( numOne > numTwo ){
+        return 1;
+      } else {
+        return 0;
+      }
+    }//end function compareNumbers
+    return compareNumbers( plans[0].date, plans[1].date );
+  }//end function groupByDate
+
+  const sortPlans = (ev)=> {
+    ev.preventDefault();
+    console.log("In sortPlans");
+    console.log(plans);
+    plans.sort(groupByDate);
+    console.log("after sort by date, plans = ",plans)
   }
 
   return (
@@ -78,8 +101,9 @@ function App() {
         <CreateEvent onChange={onChange} onSubmit={onSubmit} plan={plan}/>
 
       <h2> All {plans.length} Events:</h2>
-      <form id="future">
-        <EventList plans= { plans }/>
+      <form id="future" onSubmit={sortPlans}>
+        <EventList plans={plans} />
+        <button disabled={plans.length===0}>Sort Events</button>
       </form>
       <form id="past">
       </form>
